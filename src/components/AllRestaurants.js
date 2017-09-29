730d16254cce7667ae70b93e86bdd031c205e95f
jelenanesic <jelenanesicm@gmail.com>
2017-09-29 23:45:54 +0200

    Added loader while waiting for data

--- src/components/AllRestaurants.js
+++ src/components/AllRestaurants.js
@@ -9,8 +9,15 @@ class AllRestaurants extends Component {
     super();
     this.state = {
       search: '',
-      selectedValue: 'all' 
+      selectedValue: 'all',
+      loadPage: false
     }
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      loadPage: true
+    })
   }
 
   updateSearch(event) {
@@ -51,48 +58,52 @@ class AllRestaurants extends Component {
 
     return (
       <div className="AllRestaurants container">
-        <div className="row">
-          <div className="col-md-5 wrap-input">
-            <input
-              placeholder="Type restaurant name here"
-              className="form-control search-name" 
-              type="text"
-              value={this.state.search}
-              onChange={this.updateSearch.bind(this)}
-            />
-          </div>
-        </div> {/*end input field*/}
-        <div className="row aligner">
-          <button 
-            className="btn btn-primary btn-reset"
-            onClick={this.resetSearch.bind(this)}
-          >
-          reset
-          </button>
-        </div> {/*end reset button*/}
-        <div className="row aligner">
-          <select
-            defaultValue={this.state.selectedValue}
-            onChange={this.updateCountry.bind(this)}
-          >
-            <option value="all">All</option>
-            <option value="Serbia">Serbia</option>
-            <option value="The Netherlands">The Netherlands</option>
-          </select>
-        </div> {/*end select*/}
-        <div className="row">
-        {
-          optionSelected.length > 0 ?
-          optionSelected.map((data, index) => {
-            return(
-              <SingleRestaurant key={index} restaurantInfo={data} />
-            );
-          }) :
-          <div className="col-md-12 text-center no-results">
-            <h2>there are no search results found</h2>
-          </div>
-        }
-        </div>
+      { this.state.loadPage ?   
+      <div>
+          <div className="row">
+            <div className="col-md-5 wrap-input">
+              <input
+                placeholder="Type restaurant name here"
+                className="form-control search-name" 
+                type="text"
+                value={this.state.search}
+                onChange={this.updateSearch.bind(this)}
+              />
+            </div>
+          </div> {/*end input field*/}
+          <div className="row aligner">
+            <button 
+              className="btn btn-primary btn-reset"
+              onClick={this.resetSearch.bind(this)}
+            >
+            reset
+            </button>
+          </div> {/*end reset button*/}
+          <div className="row aligner">
+            <select
+              defaultValue={this.state.selectedValue}
+              onChange={this.updateCountry.bind(this)}
+            >
+              <option value="all">All</option>
+              <option value="Serbia">Serbia</option>
+              <option value="The Netherlands">The Netherlands</option>
+            </select>
+          </div> {/*end select*/}
+          <div className="row">
+          {
+            optionSelected.length > 0 ?
+            optionSelected.map((data, index) => {
+              return(
+                <SingleRestaurant key={index} restaurantInfo={data} />
+              );
+            }) :
+            <div className="col-md-12 text-center no-results">
+              <h2>there are no search results found</h2>
+            </div>
+          }
+          </div>  
+        </div> :
+        <h2 className="text-center"> loading . . . </h2> }
       </div>
     );
   }
